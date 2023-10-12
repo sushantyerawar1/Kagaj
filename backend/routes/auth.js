b412a64b63f5da9dcae7ac55098de4b58ee76b97
sushantyerawar1 <84599862+sushantyerawar1@users.noreply.github.com>
2023-10-13 02:20:11 +0530

    Done

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.post('/signup', signup);
 router.post('/login', login);
 router.post('/forgot-password', forgotpassword);
-// router.get('/reset-password/:id/:token', resetpassword);
+router.get('/reset-password/:id/:token', resetpassword);
 router.post('/reset-password/:id/:token', resetpassworddone);
 
 module.exports = router;
