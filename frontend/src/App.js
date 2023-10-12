981448ee1593575c76f95c4b0dfb196a51e26d0a
sushantyerawar1 <84599862+sushantyerawar1@users.noreply.github.com>
2023-10-12 23:54:41 +0530

    Added Signup Page and also Integrated with backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/HomePage/homepage"
 import Login from "./Authentication/Login"
+import SignUp from "./Authentication/Signup"
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} exact />
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   );
